39b4a6af26a984ede2be9ecd12d954dc51837bc6
paili2 <zxcv7502@naver.com>
2025-07-07 14:53:44 +0900

    chore: 요청 작업 반영

--- src/widgets/layout/sidebar/data/menuItems.tsx
+++ src/widgets/layout/sidebar/data/menuItems.tsx
@@ -12,12 +12,11 @@ import {
 export const menuItems: NavItem[] = [
   {
     icon: <Grid2X2Icon />,
-    name: "Dashboard",
+    name: "대시보드",
     path: "/",
-    // subItems: [{ name: "Ecommerce", path: "/", pro: false }],
   },
   {
-    name: "Partners",
+    name: "파트너",
     icon: <UserCircle2Icon />,
     subItems: [
       { name: "파트너사 리스트", path: "/partners" },
@@ -26,7 +25,7 @@ export const menuItems: NavItem[] = [
   },
   {
     icon: <StickyNote />,
-    name: "K-Resources",
+    name: "K리소스",
     subItems: [
       { name: "K-Resources 리스트", path: "/k-resources" },
       { name: "우리 회사 K-Resources", path: "/k-resources/me" },
@@ -34,39 +33,44 @@ export const menuItems: NavItem[] = [
   },
   {
     icon: <Folder />,
-    name: "Project List",
+    name: "관리",
     subItems: [
-      { name: "참여중인 프로젝트", path: "/project/active", ongoing: true },
-      { name: "참여한 프로젝트", path: "/project/archived" },
+      { name: "프로젝트 리스트", path: "/project/active" },
+      { name: "컨소시엄 리스트", path: "/consortium/active" },
+      { name: "계약 및 정산 목록", path: "/contracts" },
+      { name: "조직", path: "/company" },
+      { name: "내부 관리자", path: "/system/admins" },
     ],
   },
-  {
-    icon: <Rows3 />,
-    name: "Consortium List",
-    subItems: [
-      { name: "참여중인 컨소시엄", path: "/consortium/active", ongoing: true },
-      { name: "참여한 컨소시엄", path: "/consortium/archived" },
-    ],
-  },
-  {
-    icon: <Sheet />,
-    name: "Contract & Settlement List",
-    path: "/contracts",
-  },
-  {
-    icon: <Users />,
-    name: "Organization",
-    subItems: [
-      { name: "직원 관리", path: "/employees" },
-      { name: "회사 관리", path: "/company" },
-    ],
-  },
-  {
-    icon: <UserCircle2Icon />,
-    name: "Internal Admins",
-    subItems: [
-      { name: "관리자 관리", path: "/system/admins" },
-      { name: "파트너사 관리", path: "/system/partners" },
-    ],
-  },
+  // {
+  //   icon: <Folder />,
+  //   name: "Project List",
+  //   subItems: [
+  //     { name: "참여중인 프로젝트", path: "/project/active", ongoing: true },
+  //     { name: "참여한 프로젝트", path: "/project/archived" },
+  //   ],
+  // },
+  // {
+  //   icon: <Rows3 />,
+  //   name: "Consortium List",
+  //   subItems: [
+  //     { name: "참여중인 컨소시엄", path: "/consortium/active", ongoing: true },
+  //     { name: "참여한 컨소시엄", path: "/consortium/archived" },
+  //   ],
+  // },
+  // {
+  //   icon: <Sheet />,
+  //   name: "Contract & Settlement List",
+  //   path: "/contracts",
+  // },
+  // {
+  //   icon: <Users />,
+  //   name: "Organization",
+  //   path: "/company",
+  // },
+  // {
+  //   icon: <UserCircle2Icon />,
+  //   name: "Internal Admins",
+  //   path: "/system/admins",
+  // },
 ];

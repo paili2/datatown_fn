64f751962638b9687c9c07aa6f87c91c087519ce
장효환 <jalsos777@naver.com>
2025-07-09 21:45:35 +0900

    feat:Partners 회원가입 관리 페이지 작성

---
+++ app/(dashboard)/partners/pendingpartners/page.tsx
@@ -0,0 +1,9 @@
+import PendingPartners from "@/features/pendingpartners/components/PendingPartners";
+
+export default function PendingPartnersPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <PendingPartners />
+    </div>
+  );
+}
---
+++ src/features/pendingpartners/types/components.ts
@@ -0,0 +1,8 @@
+export interface PendingPartners {
+  id: string;
+  company_name: string;
+  account_type: string;
+  grade: string;
+  request_at: string;
+  primary_contact_name: string;
+}

5d8787942dff27aec64f1fafbe4cdb8e07153929
joshua <iuttn123@gmail.com>
2025-07-10 11:47:49 +0900

    refactor: 리팩토리 전 모습

--- src/features/k-resource/detail/components/TextBox.tsx
+++ src/features/k-resource/detail/components/TextBox.tsx
@@ -1,17 +1,17 @@
-import Badge from "@/shared/ui/badge/Badge";
+import Badge from '@/shared/ui/badge/Badge';
 
-type TextBoxProps ={
-    badge?: string,
-    text?:string
-}
+type TextBoxProps = {
+  badge?: string;
+  text?: string;
+};
 
-const TextBox = ({badge,text}:TextBoxProps) => {
-    return (
-            <div className="w-full h-[200px] flex flex-col bg-gray-200 font-bold p-5 relative">
-                <strong>{text}</strong>
-                <div className="absolute top-5 right-5">{ badge? <Badge>{badge}</Badge>:null}</div>
-            </div>
-                    );
-}
- 
+const TextBox = ({ badge, text }: TextBoxProps) => {
+  return (
+    <div className="w-full h-[200px] flex flex-col bg-gray-200 font-bold p-5 relative">
+      <strong>{text}</strong>
+      <div className="absolute top-5 right-5">{badge ? <Badge>{badge}</Badge> : null}</div>
+    </div>
+  );
+};
+
 export default TextBox;
